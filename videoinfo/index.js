47798268bc2fcdb04f962015cdc6f786f006b940
Carlos Torres Gonzalez <e_ctor01@mercadona.es>
2021-05-11 17:16:41 +0200

    Icon added and IPC

--- videoinfo/index.js
+++ videoinfo/index.js
@@ -1,10 +1,27 @@
 const electron = require('electron');
 const { join } = require('path');
+const ffmpeg = require('fluent-ffmpeg');
+ffmpeg.setFfprobePath("C:\\Users\\catorres\\SOFTWARE\\FFMPEG\\ffmpeg-20200831-4a11a6f-win64-static\\bin\\ffprobe.exe");
 
-const { app, BrowserWindow } = electron;
+const { app, BrowserWindow , ipcMain } = electron;
+
+let mainWindow;
 
 app.on('ready', () => {
-    const mainWindow = new BrowserWindow({});
+    mainWindow = new BrowserWindow({
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+        },
+        icon: join(__dirname, './resources/icon.png')
+    });
     mainWindow.loadURL(join(__dirname, 'index.html'));
     console.log('Application up & running');
+});
+
+ipcMain.on('video:submit', (event, path) =>{    
+    ffmpeg.ffprobe(path, (err, metadata) => {
+        console.log('Video duration is: ', metadata.format.duration);
+        mainWindow.webContents.send('video:metadata', metadata.format.duration);
+    });
 });
