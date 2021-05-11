5a6df9526257aba08a7c84c8acd42b1264e30cb6
Carlos Torres Gonzalez <e_ctor01@mercadona.es>
2021-05-11 11:58:04 +0200

    New main window

--- videoinfo/index.js
+++ videoinfo/index.js
@@ -1,1 +1,10 @@
 const electron = require('electron');
+const { join } = require('path');
+
+const { app, BrowserWindow } = electron;
+
+app.on('ready', () => {
+    const mainWindow = new BrowserWindow({});
+    mainWindow.loadURL(join(__dirname, 'index.html'));
+    console.log('Application up & running');
+});
