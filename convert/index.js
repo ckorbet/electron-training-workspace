d56ffbb1011ee0b31b8c21e7979bc7af88ddaf41
Carlos Torres Gonzalez <e_ctor01@mercadona.es>
2021-05-17 09:48:02 +0200

    Running promises in parallel with Promise.all

--- convert/index.js
+++ convert/index.js
@@ -1,6 +1,9 @@
 const { join } = require('path');
 const electron = require('electron');
 const { app, ipcMain, BrowserWindow } = electron;
+const ffmpeg = require('fluent-ffmpeg');
+const _ = require('lodash');
+ffmpeg.setFfprobePath("C:\\Users\\catorres\\SOFTWARE\\FFMPEG\\ffmpeg-20200831-4a11a6f-win64-static\\bin\\ffprobe.exe");
 
 let mainWindow;
 
@@ -9,6 +12,8 @@ app.on('ready', () => {
         webPreferences: {
             nodeIntegration: true,
             contextIsolation: false,
+            nodeIntegrationInWorker: true,
+            enableRemoteModule: true
         },
         height: 600,
         width: 800,
@@ -18,8 +23,32 @@ app.on('ready', () => {
     mainWindow.on('closed', () => app.quit());    
 
     console.log('Application up & running');
+    console.log('');
 });
 
-// ipcMain.on('update-timer', (event, timeLeft) => {
-//     timeTray.setTitle(timeLeft);
-// });
+ipcMain.on('videos:added', (event, videos) => {
+    // const promise = new Promise((resolve, reject) => {
+    //     ffmpeg.ffprobe(videos[0].path, (err, metadata) => {
+    //         resolve(metadata);
+    //     });
+    // });
+
+    // promise.then((metadata) => {
+    //     console.log(metadata);
+    // }).catch((error) => {
+    //     console.log(error);
+    // });
+
+    const promises = _.map(videos, video => {
+        return new Promise((resolve, reject) => {
+            ffmpeg.ffprobe(video.path, (err, metadata) => {
+                resolve(metadata);
+            }); 
+        });
+    });
+
+    Promise.all(promises).then(results => {
+        console.log(results);
+    });
+    
+});
